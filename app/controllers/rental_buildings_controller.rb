215206fbe1b37d164a1d0c11a5e0e38fd109850e
misaki arita <misaki.arita0838@gmail.com>
2022-12-22 16:37:34 +0900

    コントローラーの実装

---
+++ app/controllers/rental_buildings_controller.rb
@@ -0,0 +1,32 @@
+class RentalBuildingsController < ApplicationController
+  def new
+    @rental_building = RentalBuilding.new
+    @rental_building.closest_stations.build
+  end
+
+  def create
+    @rental_building = RentalBuilding.new(rental_params)
+      if @rental_building.save
+        redirect_to rental_building_path(@rental_building.id)
+      else
+        render :new
+      end
+  end
+
+  def show
+    @rental_building = RentalBuilding.find(params[:id])
+    @closest_stations = @rental_building.closest_stations
+  end
+
+ def index
+    @rental_building = RentalBuilding.all
+ end
+
+  private
+
+  def rental_params
+    params.require(:rental_building).permit(:name, :price, :age, :address, :comment, 
+                                            closest_station_attributes: [:id, :line, :station_name, :walk_by])
+  end
+
+end
